aecfa0f05da7ebd9d5c714fd84bbc227e52ef46c
CE5RIC <cedricdierckx5@hotmail.com>
2022-09-19 12:40:32 +0200

    container with images and some basic css styling for starters

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import imageDesktop from "./images/pattern-divider-desktop.svg"
+import imageMobile from "./images/pattern-divider-mobile.svg"
+import dice from "./images/icon-dice.svg"
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <h1>lorem, ipsum</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, minus!</p>
 
+      <picture>
+        <source media="(min-width: 768px)" srcSet={imageDesktop}/>
+        <img src={imageMobile} alt=""/>
+      </picture>
+        <div>
+            <button>
+                <img src={dice} alt="" />
+            </button>
+        </div>
     </div>
   );
 }
